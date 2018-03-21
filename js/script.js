94220fc3518a4d0460e181cb719ad62d872dcae0
Michał Matysiak <michalmatysiak02@gmail.com>
2018-03-21 15:05:31 +0100

    Add files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var buttonClass = document.getElementsByClassName('button');
+console.log(buttonClass);
+
+for ( var i = 0 ; i < buttonClass.length; i++) {
+  console.log(buttonClass[i].innerText);
+
+  alert(buttonClass[i].innerText);
+}
