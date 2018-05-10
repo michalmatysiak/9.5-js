891f420f21fff9764d3352eada96db008d002875
Michał Matysiak <michalmatysiak02@gmail.com>
2018-05-10 11:42:44 +0200

    Change in css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 var buttonClass = document.getElementsByClassName('button');
 console.log(buttonClass);
-
-for ( var i = 0 ; i < buttonClass.length; i++) {
-  console.log(buttonClass[i].innerText);
-
-  alert(buttonClass[i].innerText);
+for (var i = 0; i < buttonClass.length; i++) {
+    console.log(buttonClass[i].innerText);
+    alert(buttonClass[i].innerText);
 }
